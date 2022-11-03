9b62da33cd9f7d903e9713407e2821512152b0a1
Or <orlevy11@gmail.com>
2022-11-03 13:27:04 +0200

    Components done

--- src/App.js
+++ src/App.js
@@ -31,7 +31,9 @@ class Nav extends Component {
 }
 
 class LandingPage extends Component {
-  // your code here...
+  render() {
+    return <h1>Welcome!</h1>;
+  }
 }
 
 // Spot Check 3
@@ -47,19 +49,31 @@ class Profile extends Component {
 }
 
 // Spot Check 4
-export const Banner = function () {};
+
+export const Banner = () => <div className="banner">Banner</div>
 
 // Spot Check 5
 export class About extends Component {
-  /* your code here... */
+  render() {
+    return (
+      <div id="about">
+        <SignUp/>
+        <Blurb/>
+      </div>
+    );
+  }
 }
 
 export class SignUp extends Component {
-  /* your code here... */
+  render() {
+    return <p>This is an example of a SignUp page~</p>;
+  }
 }
 
 export class Blurb extends Component {
-  /* your code here... */
+  render() {
+    return <p>This is an example of a Blurb page~</p>;
+  }
 }
 
 export class App extends Component {
@@ -73,6 +87,7 @@ export class App extends Component {
           <h4 className="ex-title">Spotcheck 2</h4>
           <div className="exercise" id="spotcheck-2">
             <Nav />
+            <LandingPage />
             <Sum />
             {/* your code here... */}
           </div>
@@ -89,20 +104,22 @@ export class App extends Component {
           <h4 className="ex-title">Spotcheck 4</h4>
           <div className="exercise" id="spotcheck-4">
             {/* your code here... */}
+            <Banner/>
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className="ex-title">Spotcheck 5</h4>
           <div className="exercise" id="spotcheck-5">
-            {/* your code here... */}
+            <About/>
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className="ex-title">Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here... */}
+            {<Dummy/>}
+            
           </div>
         </div>
 
@@ -110,6 +127,7 @@ export class App extends Component {
           <h4 className="ex-title">Exercise 2</h4>
           <div className="exercise" id="ex-2">
             {/* your code here... */}
+            <Spamalot/>
           </div>
         </div>
 
@@ -117,6 +135,9 @@ export class App extends Component {
           <h4 className="ex-title">Exercise 4</h4>
           <div className="exercise" id="ex-4">
             {/* your code here... */}
+            <NavBar/>
+            <Menu/>
+            <Checkout/>
           </div>
         </div>
       </div>
--- src/components/Spam.js
+++ src/components/Spam.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
 
-const Spam =()=>{}
+const Spam =()=>{return <div>spam</div>}
 
 export default Spam
